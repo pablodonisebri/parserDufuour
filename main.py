92f67e5178e03656c6859806e22cf44b1c231c64
pablodonisebri <48103167+pablodonisebri@users.noreply.github.com>
2025-08-09 23:26:03 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from cite_categorizer.categorizer import CiteCategorizer
-
-def main():
-    palabra = input("Enter the keyword (palabra): ").strip().lower().replace(" ", "_")
-    categorizer = CiteCategorizer(palabra)
-    categorizer.run()
-
-if __name__ == "__main__":
-    main()
